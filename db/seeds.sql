f571e665521b63d092cbe5a78b246b3c0d48f8b5
Jacob Ashworth <jeashwor@gmail.com>
2020-09-09 19:39:55 -0400

    added seeds.sql file

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Tucker Meat Market - Mammoth Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('H & F Burger', TRUE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('One Eared Stag', FALSE);
