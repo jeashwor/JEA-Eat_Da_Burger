c6d59ce4e636cf608567e138d678fbf36300cdb9
Jacob Ashworth <jeashwor@gmail.com>
2020-09-11 13:32:11 -0400

    added delete function to ORM

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,31 @@
 const connection = require("../config/connection.js");
 
+// Helper function for SQL syntax.
+const printQuestionMarks = (num) => {
+    let arr = [];
+
+    for (let i = 0; i < num; i++) {
+        arr.push("?");
+    }
+
+    return arr.toString();
+}
+
+// Helper function to convert object key/value pairs to SQL syntax
+const objToSql = (ob) => {
+    let arr = [];
+    for (let key in ob) {
+        let value = ob[key];
+        if (Object.hasOwnProperty.call(ob, key)) {
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+            arr.push(key + "=" + value);
+        }
+    }
+    return arr.toString();
+}
+
 const orm = {
     selectAll: (tableInput, cb) => {
         let queryString = "SELECT * FROM ??;";
@@ -10,8 +36,15 @@ const orm = {
             cb(result);
         });
     },
-    insertOne: (text, cb) => {
-        let queryString = "INSERT INTO burgers (burger_name, devoured) VALUES (" + text + ", false);";
+    insertOne: (tableInput, cols, vals, cb) => {
+        let queryString = "INSERT INTO " + tableInput;
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+
         connection.query(queryString, (err, result) => {
             if (err) {
                 throw err;
@@ -19,8 +52,25 @@ const orm = {
             cb(result);
         });
     },
-    updateOne: (condition, cb) => {
-        let queryString = "UPDATE burgers SET WHERE " + condition + ";";
+    updateOne: (tableInput, objColVals, condition, cb) => {
+        let queryString = "UPDATE " + tableInput;
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+
+        connection.query(queryString, (err, result) => {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+    deleteOne: (tableInput, condition, cb) => {
+        let queryString = "DELETE FROM " + tableInput;
+        queryString += " WHERE ";
+        queryString += condition;
+
         connection.query(queryString, (err, result) => {
             if (err) {
                 throw err;
